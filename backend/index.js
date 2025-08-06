9f0966706cfec57f6c003f166bb0a6a24b48e5dc
naveenkumar8034 <chaudharynaveen377@gmail.com>
2025-08-06 10:53:24 +0530

    Added root route for /

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,9 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
 app.listen(PORT,()=>{
     connectDB();
